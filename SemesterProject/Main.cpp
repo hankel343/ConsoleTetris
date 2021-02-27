c5c557e77efdc34aa4c7dcee35322f013756a51f
hankel343 <57358008+hankel343@users.noreply.github.com>
2021-02-27 15:13:17 -0600

    *** Update 2/27/2021 *** -Simply added a statement that randomly generates a tetromino everytime the program is compiled.

--- SemesterProject/Main.cpp
+++ SemesterProject/Main.cpp
@@ -5,13 +5,16 @@ using namespace std;
 
 int main()
 {
-	srand(time(0));
+	srand(time(0)); //Used for random piece generation
+
+	//Creating Game Objects:
 	Board GameBoard;
 	PlayField PlayField;
 	Tetromino Pieces;
 
 	//Game Logic:
 	bool bGameOver = false;
+	Pieces.GetNewPiece(); //Get an initial piece for the first iteration.
 
 	//Game Loop:
 	while (bGameOver == false)
